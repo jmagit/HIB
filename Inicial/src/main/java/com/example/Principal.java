f541b35b14140435a06124c15a1c28d134cb8cd1
Javier <jmartin@grupoloyal.com>
2021-01-11 18:22:20 +0100

    Create & Read

--- Inicial/src/main/java/com/example/Principal.java
+++ Inicial/src/main/java/com/example/Principal.java
@@ -1,10 +1,22 @@
 package com.example;
 
+import org.hibernate.Session;
+
+import com.example.entities.Profesor;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		session.save(new Profesor(1, "Pepito", "Grillo", 99));
+		session.save(new Profesor(2, "Carmelo", "Cotos", 25));
+		session.save(new Profesor(0, "Pedro", "Pica Priedra", 41));
+		session.getTransaction().commit();
+		session.close();
+		session = HibernateUtil.getSessionFactory().openSession();
+		System.out.println(session.get(Profesor.class, 1));
+		session.close();
 	}
 
 }

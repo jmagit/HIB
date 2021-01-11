7e8bfaf8af06b5035ba52469e0b1c52fe2a633e7
Javier <jmartin@grupoloyal.com>
2021-01-11 19:14:20 +0100

    update & delete

--- Inicial/src/main/java/com/example/Principal.java
+++ Inicial/src/main/java/com/example/Principal.java
@@ -15,8 +15,68 @@ public class Principal {
 		session.getTransaction().commit();
 		session.close();
 		session = HibernateUtil.getSessionFactory().openSession();
-		System.out.println(session.get(Profesor.class, 1));
+		Profesor profesor = session.get(Profesor.class, 1);
+		if(profesor == null)
+			System.out.println("No existe el profesor");
+		else 
+			System.out.println(session.get(Profesor.class, 1));
 		session.close();
+		
+//		session = HibernateUtil.getSessionFactory().openSession();
+//		session.beginTransaction();
+//		profesor = session.get(Profesor.class, 1);
+//		if(profesor == null)
+//			System.out.println("No existe el profesor");
+//		else {
+//			profesor.setApellidos(profesor.getApellidos().toUpperCase());
+//		}
+//		session.getTransaction().commit();
+//		session.close();
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		profesor = session.get(Profesor.class, 1);
+		session.getTransaction().commit();
+		session.close();
+		
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		if(profesor == null)
+			System.out.println("No existe el profesor");
+		else {
+			System.out.println("Modifico");
+			//profesor.setId(33);
+			profesor = session.get(Profesor.class, 1);
+			profesor.setApellidos(profesor.getApellidos().toUpperCase());
+			//session.update(new Profesor(1, "PPPPepito", "Grillo", 99));
+			//session.saveOrUpdate(new Profesor(1, "PPPPepito", "Grillo", 99));
+		}
+		session.getTransaction().commit();
+		session.close();
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.createQuery("from Profesor").getResultStream()
+			.forEach(item -> System.out.println(item));
+		session.close();
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		if(profesor == null)
+			System.out.println("No existe el profesor");
+		else {
+			profesor = session.get(Profesor.class, 1);
+			profesor.setApellidos(profesor.getApellidos().toUpperCase());
+			session.delete(profesor);
+			session.delete(new Profesor(3));
+		}
+		session.getTransaction().commit();
+		session.close();
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		session.createQuery("from Profesor").getResultStream()
+			.forEach(item -> System.out.println(item));
+		session.close();
+		
 	}
 
 }
